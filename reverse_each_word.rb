f35900935d59c5b5fb789bd563756fd7cb9b98eb
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-13 23:40:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,9 @@ def reverse_each_word(sentences)
   array.each {|word| word.reverse!}
   array.join (" ")
 end
+
+def reverse_each_word(sentences)
+  array = sentences.split(" ")
+  array.collect {|word| word.reverse}
+  array.join (" ")
+end
